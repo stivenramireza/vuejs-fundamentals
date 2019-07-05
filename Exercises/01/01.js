2ad34bbc33329ad773171af380b386b011df5eae
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-07-05 17:07:00 -0500

    Adding exercise 01

---
+++ Exercises/01/01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola mundo con Vue',
+        frutas: [
+            {nombre: 'Manzana', cantidad: 10},
+            {nombre: 'Pera', cantidad: 5},
+            {nombre: 'Platano', cantidad: 0}
+        ]
+    }
+})
