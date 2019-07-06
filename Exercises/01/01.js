b6e4c75163e70732cae2a659b7e26ec74a8398d3
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-07-06 08:12:45 -0500

    Adding changes

--- Exercises/01/01.js
+++ Exercises/01/01.js
@@ -6,6 +6,26 @@ const app = new Vue({
             {nombre: 'Manzana', cantidad: 10},
             {nombre: 'Pera', cantidad: 5},
             {nombre: 'Platano', cantidad: 0}
-        ]
-    }
+        ],
+        nuevaFruta: '',
+        total: 0
+    },
+    methods: {
+        agregarFruta(){
+            this.frutas.push({
+                nombre: this.nuevaFruta,
+                cantidad: 0
+            })
+            this.nuevaFruta = '';   
+        }
+    },
+    computed: {
+        sumarFrutas(){
+            this.frutas = 0
+            for(fruta of this.frutas){
+                this.total += fruta.cantidad
+            }
+            return this.total;
+        }
+    },
 })
