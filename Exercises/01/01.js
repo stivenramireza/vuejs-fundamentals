7ad1b8df3eadffccdf6b8ab51aa44b5c82ea6085
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-07-06 08:20:00 -0500

    Exercise 01 running

--- Exercises/01/01.js
+++ Exercises/01/01.js
@@ -13,15 +13,14 @@ const app = new Vue({
     methods: {
         agregarFruta(){
             this.frutas.push({
-                nombre: this.nuevaFruta,
-                cantidad: 0
+                nombre: this.nuevaFruta, cantidad: 0
             })
             this.nuevaFruta = '';   
         }
     },
     computed: {
         sumarFrutas(){
-            this.frutas = 0
+            this.total = 0
             for(fruta of this.frutas){
                 this.total += fruta.cantidad
             }
