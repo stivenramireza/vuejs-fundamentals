880065c610f3687a46b1808323b6fbfe9f7007ae
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-07-06 09:38:21 -0500

    Adding methods and events

--- Simple_Gym_App/app.js
+++ Simple_Gym_App/app.js
@@ -1,6 +1,19 @@
 const app = new Vue({
     el: '#app',
     data: {
-        titulo: 'GYM con Vue'
+        titulo: 'GYM con Vue',
+        tareas: [],
+        nuevaTarea: ''
+    },
+    methods:{
+        agregarTarea(){
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false
+            });
+            console.log(this.tareas);
+            this.nuevaTarea = '';
+        }
     }
+
 });
