567a7d37ab1a15e005c358732d143efdabd68a15
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-07-06 10:12:28 -0500

    Simple Gym App finished

--- Simple_Gym_App/app.js
+++ Simple_Gym_App/app.js
@@ -13,7 +13,30 @@ const app = new Vue({
             });
             console.log(this.tareas);
             this.nuevaTarea = '';
+            obtenerTareas(this.tareas);
+        },
+        editarTarea(index){
+            this.tareas[index].estado = true;
+            obtenerTareas(this.tareas);
+        },
+        eliminar(index){
+            this.tareas.splice(index, 1);
+            obtenerTareas(this.tareas);
         }
-    }
+    },
+    created() {
+        let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+        if(datosDB === null){
+            this.tareas = [];
+        }else{
+            this.areas = datosDB;
+
+        }
+    },
 
 });
+
+
+function obtenerTareas(tareas){
+    localStorage.setItem('gym-vue', JSON.stringify(tareas));
+}
