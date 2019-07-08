bb8ddeced4a9531918957014a18853bf70c964d7
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-07-08 15:28:59 -0500

    Adding basic_components exercise

---
+++ Exercises/basic_components/saludo.js
@@ -0,0 +1,14 @@
+Vue.component('saludo',{
+    template: // html
+    `
+    <div>
+    <h1>{{saludo}}</h1>
+    <h3>fsfsfsffs</h3>
+    </div>
+    `,
+    data(){
+        return{
+            saludo: 'Hola desde Vue'
+        }
+    }
+})
