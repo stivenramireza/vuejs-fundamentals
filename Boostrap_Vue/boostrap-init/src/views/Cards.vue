d98ea2036cbb3cd8ee6471ddece187c1fda1631c
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-07-09 16:15:15 -0500

    Adding boostrap-vue-init exercises

---
+++ Boostrap_Vue/boostrap-init/src/views/Cards.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="mt-5">
+        <b-button variant="success">Texto</b-button>
+        <b-button variant="info">Texto</b-button>
+        <b-button variant="danger">Texto</b-button>
+        <b-button variant="primary">Texto</b-button>
+        <b-button variant="warning">Texto</b-button>
+        <b-button variant="dark">Texto</b-button>
+
+        <b-row class="mt-5">
+            <b-col cols="12" md="4">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-top
+                >   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores ab quasi, possimus at dolorum. In, eos! Voluptatibus, saepe assumenda. Cum aliquid sed laboriosam optio maiores a dolorum ullam quasi?</p>
+                    <b-button variant="dark">Texto</b-button>
+                </b-card>
+            </b-col>
+
+            <b-col cols="12" md="4">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-top
+                >   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores ab quasi, possimus at dolorum. In, eos! Voluptatibus, saepe assumenda. Cum aliquid sed laboriosam optio maiores a dolorum ullam quasi?</p>
+                    <b-button variant="dark">Texto</b-button>
+                </b-card>
+            </b-col>
+
+            <b-col cols="12" md="4">
+                <b-card
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-top
+                >   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolores ab quasi, possimus at dolorum. In, eos! Voluptatibus, saepe assumenda. Cum aliquid sed laboriosam optio maiores a dolorum ullam quasi?</p>
+                    <b-button variant="dark">Texto</b-button>
+                </b-card>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Cards'
+}
+</script>
---
+++ Boostrap_Vue/boostrap-init/src/views/Form.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="mt-5">
+        <h1>Formularios</h1>
+
+        <form>
+            <b-row>
+                <b-col md="3">
+                    <b-form-input type="text" v-model="texto" :state="comprobar"></b-form-input>
+                    <small>Mínimo 3 caracteres</small>
+                </b-col>
+
+                <b-col md="3">
+                    <b-form-select v-model="select" :options="animales"></b-form-select>
+                    <p>Selección: {{select}}</p>
+                </b-col>
+                <b-col md="3">
+                    <b-form-radio-group v-model="radio" :options="animalesRadio"></b-form-radio-group>
+                    <p>Radio: {{radio}}</p>
+                </b-col>
+                <b-col md="3">
+                    <b-form-checkbox-group v-model="check" :options="animalesRadio"></b-form-checkbox-group>
+                    <p>Selected: {{check}}</p>
+                </b-col>
+            </b-row>
+            
+        </form>
+        <p>texto: {{texto}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Form',
+    data(){
+        return{
+            texto: '',
+            check: [],
+            select: null,
+            radio: null,
+            animales: [
+                {value: null, text: 'Seleccione un animal'},
+                {value: 'perro', text: 'Guau guau'},
+                {value: 'gato', text: 'Miau'},
+                {value: 'pato', text: 'Cuack'}
+            ],
+            animalesRadio: [
+                {value: 'perro', text: 'Guau guau'},
+                {value: 'gato', text: 'Miau'},
+                {value: 'pato', text: 'Cuack'}
+            ]
+        }
+    },
+    computed: {
+        comprobar(){
+            return this.texto.length > 2 ? true : false
+        }
+    },
+}
+</script>
