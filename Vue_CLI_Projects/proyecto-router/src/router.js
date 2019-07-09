3f0a85aed2a44878e47589e2d66259777b442659
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-07-09 11:06:13 -0500

    Adding vue_router project

---
+++ Vue_CLI_Projects/proyecto-router/src/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import(/* webpackChunkName: "home" */ './views/Home.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/services',
+      name: 'services',
+      component: () => import(/* webpackChunkName: "services" */ './views/Services.vue')
+    }
+  ]
+})
