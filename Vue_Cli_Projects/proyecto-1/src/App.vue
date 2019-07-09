42484a620dd3170909fb28e07c5b446f8d944d0c
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-07-09 10:34:23 -0500

    Adding Vue CLI components

--- Vue_Cli_Projects/proyecto-1/src/App.vue
+++ Vue_Cli_Projects/proyecto-1/src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <Cabecera></Cabecera>
+    <Lista></Lista>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cabecera from './components/Cabecera.vue'
+import Lista from './components/Lista.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Cabecera,
+    Lista
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- Vue_Cli_Projects/proyecto-1/src/store.js
+++ Vue_Cli_Projects/proyecto-1/src/store.js
@@ -5,10 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    frutas: [
+      {nombre: 'Manzana', cantidad: 0},
+      {nombre: 'Pera', cantidad: 0},
+      {nombre: 'Naranja', cantidad: 0}
+    ]
   },
   mutations: {
-
+    aumentar(state, index){
+      state.frutas[index].cantidad++
+    },
+    reiniciar(state){
+      state.frutas.forEach(elemento => {
+        elemento.cantidad = 0
+      })
+    }
   },
   actions: {
 
